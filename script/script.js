6ebde658479ef2db99f28aa22f5ad8d9680146c8
lazInsight <krajkovk@gmail.com>
2021-09-30 17:31:53 +0300

    Add adaptive design

---
+++ script/script.js
@@ -0,0 +1,8 @@
+// const prevBtn = document.querySelector('#btn-prev');
+// const nextBtn = document.querySelector('#btn-next');
+// const sliderTrack = document.querySelector('#slider-track');
+// const { width } = sliderTrack.getBoundingClientRect();
+
+// prevBtn.addEventListener('click', () => {changePosition('prev')});
+// nextBtn.addEventListener('click', () => {changePosition('next')});
+
